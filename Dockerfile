12b91fe126bbc55fed4f683bd48adabda6472498
Nic Anderson <nic@nic-a.net>
2023-05-22 17:39:22 -0400

    Starter twitch resource

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,28 @@
 ARG base_image=alpine:latest
+ARG twitchdl_version=v1.1.19
 
 FROM ${base_image} AS resource
 
 RUN apk update & apk upgrade
 RUN apk --no-cache add \
     bash \
-    curl
+    curl \
+    jq
 
 ADD assets /opt/resource
 RUN chmod +x /opt/resource/*
+
+FROM resource AS builder
+WORKDIR /root
+RUN apk --no-cache add go \
+    && curl -L -o twitch-cli.tar.gz https://github.com/twitchdev/twitch-cli/archive/refs/tags/${twitchdl_version}.tar.gz \
+    && tar -x -f twitch-cli.tar.gz
+WORKDIR /root/twitch-cli
+RUN go build --ldflags "-s -w -X main.buildVersion=source"
 
 FROM resource AS tests
 ADD test/ /tests
 RUN /tests/all.sh
 
 FROM resource
+COPY --from builder /root/twitch-cli/build/twitch /usr/local/bin/twitch
--- assets/common.sh
+++ assets/common.sh
@@ -1,1 +1,32 @@
 export TMPDIR=${TMPDIR:-/tmp}
+
+configure_credentials() {
+    # Determine our "home" for twitch-cli
+    # This shouldn't need to change, but may help with troubleshooting idk
+    twitch_home=$(jq -r '.source.home // empty' <<< "$1")
+    if [ -z $twitch_home ]; then
+        twitch_home=~/.config/twitch-cli
+    fi
+    mkdir -p $twitch_home
+
+    # Extract clientid / clientsecret from params
+    twitch_clientid=$(jq -r '.source.client_id // empty' <<< "$1")
+    twitch_clientsecret=$(jq -r '.source.client_secret // empty' <<< "$1")
+
+    if [ -z $twitch_clientid ] || [ -z $twitch_clientsecret ]; then
+        echo "You need to specify 'source.client_id' and 'source.client_secret'."
+        exit 1
+    fi
+
+    # Write basic credentials, with intentionally blank/expired token
+    # Maybe someday I can figure out how to persist this?
+    cat > $twitch_home/.twitch-cli.env <<EOF
+TOKENEXPIRATION=2020-01-01T00:00:00.0Z
+CLIENTID=${twitch_clientid}
+CLIENTSECRET=${twitch_clientsecret}
+ACCESSTOKEN=
+REFRESHTOKEN=
+TOKENSCOPES=[]
+EOF
+    twitch token
+}
